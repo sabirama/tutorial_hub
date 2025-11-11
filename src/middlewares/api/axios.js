de4072b407abaa2afaf99fa8444219d904ebef39
sabirama <markcondes21@gmail.com>
2025-11-11 13:39:42 +0800

    api

--- src/middlewares/api/axios.js
+++ src/middlewares/api/axios.js
@@ -2,10 +2,57 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
+const apiCall = async (config) => {
+    const { method = "get", url, data, headers = {}, options = {} } = config;
 
-export default api;
+    if (!url) {
+        throw new Error("URL is required for API call");
+    }
+
+    try {
+        const axiosConfig = {
+            headers: { ...headers,
+                "App-Key": import.meta.env.VITE_API || 'fallback-key' },
+            ...options 
+        };
+
+        let response;
+
+        switch (method.toLowerCase()) {
+            case "get":
+                response = await api.get(url, axiosConfig);
+                break;
+
+            case "post":
+                response = await api.post(url, data, axiosConfig);
+                break;
+
+            case "put":
+                response = await api.put(url, data, axiosConfig);
+                break;
+
+            case "delete":
+                response = await api.delete(url, axiosConfig);
+                break;
+
+            default:
+                throw new Error(`Unsupported HTTP method: ${method}`);
+        }
+
+        return response;
+    } catch (error) {
+        console.error(`API call failed [${method.toUpperCase()} ${url}]:`, error);
+        
+        const apiError = {
+            message: error.response?.data?.message || error.message,
+            status: error.response?.status,
+            data: error.response?.data,
+        };
+        
+        throw apiError;
+    }
+}
+
+export default apiCall;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "./modules/Hero"
 import Roles from "./modules/Roles"
 import "../../assets/css/Home.css"
+import apiCall from "../../middlewares/api/axios"
 
 const Home = () => {
     return (
         <div className="page">
-           <Hero/>
-           <Roles/>
+            <Hero />
+            <button onClick={() => apiCall({ method: 'post', url: '/', data: {'name': 'waner'}, headers:{'Token': 'test'} })}>API CHECK</button>
+            <Roles />
         </div>
     )
 }
